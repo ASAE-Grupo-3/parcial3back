4d5faca6a906fe9989e4b0f1ad83376af4677d08
JavierStivenDuranAlvear <javierda@unicauca.edu.co>
2022-12-20 21:34:23 -0500

    Codigos HTTP Docente

--- src/main/java/co/edu/unicauca/asae/core/proyecto/controllers/DocenteRestController.java
+++ src/main/java/co/edu/unicauca/asae/core/proyecto/controllers/DocenteRestController.java
@@ -1,12 +1,14 @@
 
 package co.edu.unicauca.asae.core.proyecto.controllers;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -37,34 +39,80 @@ public class DocenteRestController {
 	}
 
 	@GetMapping("/docentes/{id}")
-	public DocenteDTO show(@Positive(message = "{consultar.recurso.identificacion}") @PathVariable Integer id) {
+	public ResponseEntity<?> show(@Positive(message = "{consultar.recurso.identificacion}") @PathVariable Integer id) {
 		DocenteDTO objDocente = null;
-		objDocente = docenteService.findById(id);
-		return objDocente;
+		HashMap<String, Object> respuestas = new HashMap<>();
+		ResponseEntity objRespuesta;
+
+		try{
+			objDocente = docenteService.findById(id);
+			if(objDocente == null){
+				respuestas.put("mensaje", "El docente con el ID "+id+" no existe en la bd");
+				objRespuesta = new ResponseEntity<>(respuestas, HttpStatus.NOT_FOUND);
+			}else{
+				objRespuesta = new ResponseEntity<>(objDocente, HttpStatus.OK);
+			}
+		}catch(DataAccessException e){
+			respuestas.put("mensaje", "Error al realizar la consulta en la base de datos");
+			respuestas.put("Descripcion del error", e.getMessage());
+			objRespuesta = new ResponseEntity<>(respuestas, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return objRespuesta;
 	}
 
 	@PostMapping("/docentes")
-	public DocenteDTO create(@Valid @RequestBody DocenteDTO cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody DocenteDTO cliente) {
 		DocenteDTO objDocente = null;
-		objDocente = docenteService.save(cliente);
-		return objDocente;
+		HashMap<String, Object> respuestas = new HashMap<>();
+		ResponseEntity objRespuesta;
+		try{
+			objDocente = docenteService.save(cliente);
+			objRespuesta = new ResponseEntity<DocenteDTO>(objDocente, HttpStatus.CREATED);
+		}catch(DataAccessException e){
+			respuestas.put("mensaje", "Error al realizar la insercion del recurso");
+			respuestas.put("Descripcion del error", e.getMessage());
+			objRespuesta = new ResponseEntity<>(respuestas, HttpStatus.BAD_REQUEST);
+		}
+		
+		return objRespuesta;
 	}
 
 	@PutMapping("/docentes/{id}")
-	public DocenteDTO update(@RequestBody DocenteDTO cliente, @PathVariable Integer id) {
+	public ResponseEntity<DocenteDTO> update(@RequestBody DocenteDTO cliente, @PathVariable Integer id) {
 		DocenteDTO objDocente = null;
+		ResponseEntity objRespuesta;
 		System.out.println("actualizando cliente");
 		DocenteDTO docenteActual = docenteService.findById(id);
 		if (docenteActual != null) {
 			objDocente = docenteService.update(id, cliente);
 		}
-		return objDocente;
+		objRespuesta = new ResponseEntity<DocenteDTO>(objDocente, HttpStatus.OK);
+		return objRespuesta;
 	}
 
 	@DeleteMapping("/docentes/{id}")
-	public Boolean delete(@PathVariable Integer id) {
-		return this.docenteService.delete(id);
+	public ResponseEntity<?> delete(@PathVariable Integer id) {
+		DocenteDTO objDocente = null;
+		HashMap<String, Object> respuestas = new HashMap<>();
+		ResponseEntity objRespuesta;
 
+		try{
+			objDocente = docenteService.findById(id);
+			
+			if(objDocente == null){
+				respuestas.put("mensaje", "El doncente con id "+id+" que desea eliminar no esta registrado");
+				objRespuesta = new ResponseEntity<>(respuestas, HttpStatus.NOT_FOUND);
+			}else{
+				docenteService.delete(id);
+				objRespuesta = new ResponseEntity<>(true, HttpStatus.OK);
+			}
+		}catch(DataAccessException e){
+			respuestas.put("mensaje", "Error al realizar la eliminacion del recurso");
+			respuestas.put("Descripcion del error", e.getMessage());
+			objRespuesta = new ResponseEntity<>(respuestas, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return objRespuesta;
 	}
 
 }
