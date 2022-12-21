db33e21252d285bc5a0d36ea0f8d328982a528b0
kfbastidas <kevithchi@unicauca.edu.co>
2022-12-21 00:05:59 -0500

    .

--- src/main/java/co/edu/unicauca/asae/core/proyecto/repositories/AsignaturaRepository.java
+++ src/main/java/co/edu/unicauca/asae/core/proyecto/repositories/AsignaturaRepository.java
@@ -1,9 +1,12 @@
 package co.edu.unicauca.asae.core.proyecto.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.unicauca.asae.core.proyecto.models.AsignaturaEntity;
+import co.edu.unicauca.asae.core.proyecto.models.DocenteEntity;
 
 public interface AsignaturaRepository extends CrudRepository<AsignaturaEntity, Integer>{
-    
+	public List<AsignaturaEntity> findBynombre(String nombre);
 }
