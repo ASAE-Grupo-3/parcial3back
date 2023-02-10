782c79262f2e0f582318e4cd9a420b542244f85c
kfbastidas <kevithchi@unicauca.edu.co>
2023-02-10 09:25:57 -0500

    .

--- src/main/java/co/edu/unicauca/asae/core/proyecto/controllers/AsignaturaRestController.java
+++ src/main/java/co/edu/unicauca/asae/core/proyecto/controllers/AsignaturaRestController.java
@@ -11,17 +11,12 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import co.edu.unicauca.asae.core.proyecto.services.DTO.AsignaturaDTO;
 import co.edu.unicauca.asae.core.proyecto.services.services.clienteServices.IAsignaturaService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 @Validated
--- src/main/java/co/edu/unicauca/asae/core/proyecto/controllers/CursoRestController.java
+++ src/main/java/co/edu/unicauca/asae/core/proyecto/controllers/CursoRestController.java
@@ -1,24 +1,25 @@
 package co.edu.unicauca.asae.core.proyecto.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import co.edu.unicauca.asae.core.proyecto.services.DTO.CursoDTO;
 import co.edu.unicauca.asae.core.proyecto.services.services.clienteServices.ICursoService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 @Validated
@@ -61,4 +62,32 @@ public class CursoRestController {
 	public Boolean delete(@PathVariable Integer id) {
 		return this.CursoService.delete(id);
 	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, List<String>> result = new HashMap<>();
+		Map<String, String> errors = new HashMap<>();
+		List<String> listErrors = new ArrayList<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+
+			errors.put(fieldName,errorMessage);
+
+		});
+		for (Map.Entry<String, String> entry : errors.entrySet()) {
+			String stringError = "Campo '"+entry.getKey()+" ' "+entry.getValue();
+			listErrors.add(stringError);
+			//System.out.println("clave=" + entry.getKey() + ", valor=" + entry.getValue());
+		}
+
+
+
+		result.put("errors", listErrors);
+
+		return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+	}
+
 }
